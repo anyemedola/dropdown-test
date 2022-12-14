2c9094f21d34242e5a7992ed1286ffcfdb14a13f
Any Medola <anynhamedola@gmail.com>
2022-12-13 23:15:28 -0300

    submenu dropdown

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -3,16 +3,39 @@ import React from 'react';
 import profile_picture from '../.././assets/profile_picture.jpg'
 
 function Sidebar() {
-  return (
-    <div className="sidebar-container">
+    const handleClick = event => {
+        // 👇️ toggle class on click
+        event.currentTarget.classList.toggle('active');
 
-        
-<a href="https://aeatech.com.br/" target="_blank">A&A Tech</a> |
-        <a href="#">Dashboard</a> |
-        <a href="https://github.com/anyemedola/dropdown-test" target="_blank">README.md</a>
+        // 👇️ add class on click
+        // event.currentTarget.classList.add('bg-salmon');
 
-    </div>
-  );
+        // 👇️ remove class on click
+        // event.currentTarget.classList.remove('bg-salmon');
+    };
+
+    return (
+        <div className="sidebar-container">
+
+            <div class="menu-inner">
+
+                <ul>
+                    <li><a href="https://aeatech.com.br/" target="_blank">A&A Tech</a> |</li>
+                    <li onClick={handleClick}>
+                        <a href="#">Dashboard</a>
+
+                        <ul className='submenu'>
+                            <li>Abacate</li>
+                            <li>Abacaxi</li>
+                            <li>Amora</li>
+                        </ul>
+                    </li>
+                    <li><a href="https://github.com/anyemedola/dropdown-test" target="_blank">README.md</a></li>
+                </ul>
+            </div>
+
+        </div>
+    );
 }
 
 export default Sidebar;
